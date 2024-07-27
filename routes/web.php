621397560d2f645ce4a847da246013fccd1d73ec
Muhirsan <muhirsanbusines@gmail.com>
2024-07-27 23:01:27 +0700

    Membuat Route untuk menghandle halaman jika user bertype 'Admin'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Route untuk pengondisian jika user yang login bertype admin
+Route::get('admin/dashboard', [HomeController::class,'index'])->middleware(['auth','admin']);
