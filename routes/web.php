ed26cac991cffe445c2690344a0002e16a20f621
Muhirsan <muhirsanbusines@gmail.com>
2024-07-28 00:06:17 +0700

    Mengubah Route ('/') ke home.index

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
