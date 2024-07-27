4236be5e7dfc55c7233026da21b9dbe17acccd03
Muhirsan <muhirsanbusines@gmail.com>
2024-07-27 23:02:59 +0700

    Mendaftarkan alias admin pada middleware untuk menghandle route dashboard admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        
+        // Mendaftarkan alias 'admin' untuk middleware Admin
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
