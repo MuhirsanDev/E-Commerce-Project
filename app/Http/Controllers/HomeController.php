a8e8920fa637eb47ae89f6876016c4b9a51f33e3
Muhirsan <muhirsanbusines@gmail.com>
2024-07-28 00:11:25 +0700

    Menambahkan Controller Route ('/') redirect to home.index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    // Controller route 'admin.index' => 'admin/dashboard'
     public function index(){
         return view('admin.index');
+    }
+
+    // Controller route 'home.index' => '/'
+    public function home(){
+        return view('home.index');
     }
 }
