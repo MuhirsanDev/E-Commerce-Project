44dfe9222d78b5e9d21dcff4bfde4621948da7cc
Muhirsan <muhirsanbusines@gmail.com>
2024-07-30 22:09:57 +0700

    Merubah drop down product menjadi Add product

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -16,9 +16,9 @@
                 <a href="{{ url('view_category') }}"> <i class="icon-grid"></i>Category </a>
               </li>
               
-              <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
+              <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Products</a>
                 <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                  <li><a href="#">Page</a></li>
+                  <li><a href="{{ url('add_product') }}">Add Product</a></li>
                   <li><a href="#">Page</a></li>
                   <li><a href="#">Page</a></li>
                 </ul>
